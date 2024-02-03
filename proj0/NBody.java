418cce098fe1e09cd6c3f74798fc9baff4f407f6
lw <liweieternity@163.com>
2024-02-03 11:59:15 +0800

    static NBody done

--- proj0/NBody.java
+++ proj0/NBody.java
@@ -41,29 +41,33 @@ public class NBody {
         double radius = NBody.readRadius(path);
         Planet[] ps = NBody.readPlanets(path);
 
-        /*
+        
         StdDraw.setScale(-radius, radius);
         StdDraw.clear();
 
         StdDraw.picture(0, 0, background);
 
-        StdDraw.show();
-        StdDraw.pause(5000);
-         */
-        String imageToDraw = "examples/advice.png";
+        for (int i = 0; i < ps.length; i++) {
+            StdDraw.picture(ps[i].xxPos, ps[i].yyPos, "images/" + ps[i].imgFileName);
+        }
 
-        StdDraw.setScale(-100, 100);
-
-        /* Clears the drawing window. */
-        StdDraw.clear();
-
-        /* Stamps three copies of advice.png in a triangular pattern. */
-        StdDraw.picture(0, 75, imageToDraw);
-        StdDraw.picture(-75, -75, imageToDraw);
-        StdDraw.picture(75, -75, imageToDraw);
-
-        /* Shows the drawing to the screen, and waits 2000 milliseconds. */
         StdDraw.show();
         StdDraw.pause(2000);
+        
+        // String imageToDraw = "examples/advice.png";
+
+        // StdDraw.setScale(-100, 100);
+
+        /* Clears the drawing window. */
+        // StdDraw.clear();
+
+        /* Stamps three copies of advice.png in a triangular pattern. */
+        // StdDraw.picture(0, 75, background);
+        // StdDraw.picture(-75, -75, background);
+        // StdDraw.picture(75, -75, imageToDraw);
+
+        /* Shows the drawing to the screen, and waits 2000 milliseconds. */
+        // StdDraw.show();
+        // StdDraw.pause(2000);
     }
 }
