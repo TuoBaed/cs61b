fb5b3f87f59e2b4c90b594e0b64843f106878676
lw <liweieternity@163.com>
2024-02-04 14:18:57 +0800

    project1b done

---
+++ proj1b/ArrayDeque.java
@@ -0,0 +1,115 @@
+
+public class ArrayDeque<T> implements Deque<T> {
+    private T[] items;
+    private int size;
+    private int nextFirst;
+    private int nextLast;
+
+    public ArrayDeque() {
+        items = (T[]) new Object[8];
+        // addFirst 下一个添加位置在 3
+        // addLast 下一个添加位置在 4
+        nextLast = items.length / 2;
+        nextFirst = nextLast - 1;
+        size = 0;
+    }
+
+//    public boolean isEmpty() { return size == 0; }
+
+    public void resize(int newLength) {
+        T[] newItems = (T[]) new Object[newLength];
+
+        int start = (nextFirst + 1) % items.length;
+        int count = 0;
+        while (count != size) {
+            newItems[count] = items[start];
+            count++;
+            start = (start + 1) % items.length;
+        }
+        items = newItems;
+        nextFirst = newItems.length - 1;
+        nextLast = count % newItems.length;
+    }
+
+    @Override
+    public void addFirst(T x) {
+        if (size == items.length) {
+            resize(size * 2);
+        }
+        items[nextFirst] = x;
+        nextFirst = (nextFirst - 1 + items.length) % items.length;
+        size++;
+    }
+
+    @Override
+    public void addLast(T x) {
+        if (size == items.length) {
+            resize(size * 2);
+        }
+        items[nextLast] = x;
+        nextLast = (nextLast + 1) % items.length;
+        size++;
+    }
+
+    @Override
+    public int size() {
+        return size;
+    }
+
+    @Override
+    public void printDeque() {
+        int count = size;
+        int next = (nextFirst + 1) % items.length;
+
+        while (count != 0) {
+            System.out.print(items[next] + " ");
+            count--;
+            next = (next + 1) % items.length;
+        }
+        System.out.println();
+    }
+
+    @Override
+    public T get(int index) {
+        int start = (nextFirst + 1) % items.length;
+        int count = 0;
+        while (count < size) {
+            if (count == index) {
+                return items[start];
+            }
+            count++;
+            start = (start + 1) % items.length;
+        }
+        return null;
+    }
+
+    @Override
+    public T removeFirst() {
+        if (size < items.length / 4 && items.length >= 16) {
+            resize(items.length / 4);
+        }
+
+        if (size < 1) {
+            return null;
+        }
+        nextFirst = (nextFirst + 1) % items.length;
+        T removeItem = items[nextFirst];
+        size--;
+        return removeItem;
+    }
+
+    @Override
+    public T removeLast() {
+        if (size <= items.length / 4 && items.length >= 16) {
+            resize(items.length / 4);
+        }
+
+        if (size < 1) {
+            return null;
+        }
+        nextLast = (nextLast - 1 + items.length) % items.length;
+        T removeItem = items[nextLast];
+        size--;
+        return removeItem;
+    }
+}
---
+++ proj1b/Palindrome.java
@@ -0,0 +1,32 @@
+
+public class Palindrome {
+    public Deque<Character> wordToDeque(String word) {
+        LinkedListDeque<Character> n = new LinkedListDeque<>();
+        for (int i = 0; i < word.length(); i++) {
+            n.addLast(word.charAt(i));
+        }
+        return n;
+    }
+
+    public boolean isPalindrome(String word) {
+        int length = word.length();
+        for (int i = 0; i < length / 2; i++) {
+            if (word.charAt(i) != word.charAt(length - i - 1)) {
+                return false;
+            }
+            ;
+        }
+        return true;
+    }
+
+    public boolean isPalindrome(String word, CharacterComparator cc) {
+        int length = word.length();
+        for (int i = 0; i < length / 2; i++) {
+            if (!cc.equalChars(word.charAt(i), word.charAt(length - i - 1))) {
+                return false;
+            }
+            ;
+        }
+        return true;
+    }
+}
